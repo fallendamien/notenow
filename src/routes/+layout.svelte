1c86637cf392519de16bdaac225ecc219270dd10
fallendamien <11881550+fallendamien@users.noreply.github.com>
2025-09-01 23:27:58 +0800

    Add nested layouts for About section
    Introduces a dedicated layout for the /about route with its own navigation and styling. The root layout now conditionally renders content based on the current route. Also adds a Team page under /about/team.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { page } from '$app/state';
 
 	let { children } = $props();
 </script>
@@ -9,13 +10,19 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<nav class="my-2 bg-amber-600 p-2">
-	<ul class="flex gap-2">
-		<li>
-			<a href="/">Home</a>
-			<a href="/about">About</a>
-		</li>
-	</ul>
-</nav>
-
-{@render children?.()}
+{#if page.url.pathname.startsWith('/about')}
+	{@render children()}
+{:else}
+	<div class="border-2 bg-pink-900 p-4">
+		<h1 class="text-3xl">Root Layout</h1>
+		<nav class="my-2 bg-amber-600 p-2">
+			<ul class="flex gap-2">
+				<li>
+					<a href="/">Home</a>
+					<a href="/about">About</a>
+				</li>
+			</ul>
+		</nav>
+		{@render children?.()}
+	</div>
+{/if}
---
+++ src/routes/about/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import type { LayoutProps } from './$types';
+
+	let { data, children }: LayoutProps = $props();
+
+	console.log(page);
+</script>
+
+<div class="border-2 bg-purple-700 p-4">
+	<h1 class="mb-2 text-3xl">About Layout</h1>
+	<div class="flex gap-4">
+		<div class="w-[200px] bg-amber-700">
+			<nav class="p-2">
+				<ul>
+					<li class:underline={page.url.pathname === '/about'}><a href="/about">About</a></li>
+					<li class:underline={page.url.pathname === '/about/team'}>
+						<a href="/about/team">Team</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+		<div class="flex-1">
+			{@render children()}
+		</div>
+	</div>
+</div>
