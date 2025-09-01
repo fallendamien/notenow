c16c8e262253e6e0ac30a0089027cb5f9995a57a
fallendamien <11881550+fallendamien@users.noreply.github.com>
2025-09-01 17:27:30 +0800

    Add blog and about pages, button component, and DaisyUI
    Introduces new blog and about routes with corresponding Svelte pages, adds a reusable Button component, and updates the main layout with navigation links. Integrates DaisyUI with Tailwind in app.css and package files, and sets up a SvelteKit alias for components. Minor improvements to the home page to use the new Button component.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,4 +9,13 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<nav class="my-2 bg-amber-600 p-2">
+	<ul class="flex gap-2">
+		<li>
+			<a href="/">Home</a>
+			<a href="/about">About</a>
+		</li>
+	</ul>
+</nav>
+
 {@render children?.()}
---
+++ src/routes/blog/[id]/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { page } from '$app/state';
+
+	// let pageId = page.params.id;
+	let pageId = $derived(page.params.id);
+</script>
+
+<h1 class="text-4xl">Single Blog: {pageId}</h1>
+<a href="/blog/123">Another Text</a>
