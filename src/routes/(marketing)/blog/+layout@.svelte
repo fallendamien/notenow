2aafd2c5fd7070cb41926daef57f664d1b76fc30
fallendamien <11881550+fallendamien@users.noreply.github.com>
2025-09-03 09:31:44 +0800

    Add blog and blog post layouts, rename blog page
    Introduces new layout components for the blog and individual blog post routes to provide distinct styling. Also renames the blog post page component to +page@blog.svelte for clarity or routing purposes.

---
+++ src/routes/(marketing)/blog/+layout@.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { LayoutProps } from './$types';
+
+	let { data, children }: LayoutProps = $props();
+</script>
+
+<div class="border-3 bg-indigo-700 p-4">
+	<h1 class="text-3xl">(blog) Layout</h1>
+
+	{@render children?.()}
+</div>
---
+++ src/routes/(marketing)/blog/[id]/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { LayoutProps } from './$types';
+
+	let { data, children }: LayoutProps = $props();
+</script>
+
+<div class="border-3 bg-blue-950 p-4">
+	<h1 class="text-3xl">(blog/[id])</h1>
+
+	{@render children?.()}
+</div>
